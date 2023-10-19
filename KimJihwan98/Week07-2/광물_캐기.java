20593e80a2dbf335dd058db3b75105ab199e8c2b
KimJihwan98 <wlghks98k@gmail.com>
2023-10-19 12:25:36 +0900

    🛠️ Fix : (Week07) Programmers 광물_캐기

--- KimJihwan98/Week07-2/광물_캐기.java
+++ KimJihwan98/Week07-2/광물_캐기.java
@@ -1,80 +1,46 @@
 import java.util.*;
+
 class Solution {
-    static int pick_num, answer;
-    static boolean[] visited;
-    static String[] pick_order, result;
-    static HashMap<Integer, String> pick_index;
-    static HashMap<String, Integer> mineral_index; 
-    static HashMap<String, int[]> pirodo;
+    static int[][] pirodo = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+    static int pn, mn, need_pn, answer;
+    static HashMap<String, Integer> m_index;
     public int solution(int[] picks, String[] minerals) {
+        m_index = new HashMap<>();
+        m_index.put("diamond", 0);
+        m_index.put("iron", 1);
+        m_index.put("stone", 2);
+            
         answer = Integer.MAX_VALUE;
-        // 곡괭이별 인덱스
-        pick_index  = new HashMap<>();
-        pick_index.put(0, "diamond");
-        pick_index.put(1, "iron");
-        pick_index.put(2, "stone");
-        //피로도
-        pirodo =  new HashMap<>();
-        pirodo.put("diamond", new int[] {1, 1, 1});
-        pirodo.put("iron", new int[] {5, 1, 1});
-        pirodo.put("stone", new int[] {25, 5, 1});
-        // 광물 별 인덱스
-        mineral_index = new HashMap<>();
-        mineral_index.put("diamond", 0);
-        mineral_index.put("iron", 1);
-        mineral_index.put("stone", 2);
         
-        //곡괭이 개수
-        pick_num = 0;
+        mn = minerals.length;
+        pn = 0;
+        need_pn = mn/5 +1;
+        
         for(int i = 0; i < 3; i++) {
-            pick_num += picks[i];
-        }
-        //곡괭이 배열 초기화
-        pick_order = new String[pick_num];
-        int idx = 0;
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < picks[i]; j++) {
-                pick_order[idx++] = pick_index.get(i);
-            }
+            pn += picks[i];
         }
         
-        result = new String[pick_num];
-        visited = new boolean[pick_num];
-        perm(0, minerals);
-        //System.out.println(Arrays.toString(pick_order));
+        dfs(0, 0, 0, picks, minerals);
         return answer;
     }
-    
-    static void perm(int idx, String[] minerals) {
-        if(idx == pick_num) {
-            int tmp_pirodo = get_pirodo(result, minerals);
-            answer = Math.min(answer, tmp_pirodo);
+    static int[] need_picks;
+    static void dfs(int p_num, int midx, int result, int[] picks, String[] minerals) {
+        if(result > answer) return;
+        if(p_num == pn || p_num > need_pn || midx >= mn) {
+            answer = Math.min(answer, result);
         }
         
-        for(int i = 0; i < pick_num; i++) {
-            if(visited[i]) continue;
-            result[idx] = pick_order[i];
-            visited[i] = true;
-            perm(idx+1, minerals);
-            visited[i] = false;
+        for(int i = 0; i < 3; i++) {
+            if(picks[i] == 0) continue;
+            picks[i]--;
+            int tmp_pirodo = 0;
+            for(int j = 0; j < 5; j++) {
+                if(midx+j >= mn) break;
+                tmp_pirodo += pirodo[i][m_index.get(minerals[midx+j])];
+            }
+            dfs(p_num+1, midx+5, result+tmp_pirodo, picks, minerals);
+            picks[i]++;
         }
     }
     
-    static int get_pirodo(String[] picks_order, String[] minerals) {
-        int pn = pick_num;
-        int mn = minerals.length;
-        int p_idx = 0;
-        int m_idx = 0;
-        int tmp_pirodo = 0;
-        while(true) {
-            String pick = picks_order[p_idx++];
-            //System.out.println("곡괭이 : " + pick);
-            for(int i = 0; i < 5; i++) {
-                int min_index = mineral_index.get(minerals[m_idx++]);
-                tmp_pirodo += pirodo.get(pick)[min_index];
-                if(m_idx == mn) return tmp_pirodo;
-            }
-            if(p_idx == pn) return tmp_pirodo;
-        }
-    }
 }
