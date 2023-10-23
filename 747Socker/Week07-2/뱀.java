776f713b372f9d09a44a50a58528f5679211e0a5
윤길재 <yungj@yungiljaeui-MacBookAir.local>
2023-10-23 14:29:16 +0900

    🛠️ Fix : (Week07-2) 3190 뱀

--- 747Socker/Week07-2/뱀.java
+++ 747Socker/Week07-2/뱀.java
@@ -1,12 +1,18 @@
 package pract2;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class bj_re_뱀_3190 {
+public class bj_뱀_3190 {
+	static int N,K;
+	static int[][] map;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,-1,0,1};
+	static int dir;
 	
 	static class Point{
 		int x;
@@ -17,116 +23,69 @@ public class bj_re_뱀_3190 {
 			this.y = y;
 		}
 	}
-	static Deque<String> dq = new ArrayDeque<>();
-	static int N;
-	static int K;
-	static int L;
-	static int[][] map;
-	static int dx[]= {0,-1,1,0};
-	static int dy[]= {1,0,0,-1};
-	static boolean chk[][];
-	static boolean flag;
-	static Queue<Point> fQ;
-	static int[][] 	arr;
-	static int ans;
-	static int sec;
-	static int cx;
-	static int cy;
-	static int dir;
 	
-	static void move(int X, char C, int x, int y) {
-		if(!flag) return;
-		map[x][y]=2;
-		while(sec<X) {
-			if(dir==0) {
-				int nx = x--;
-				if(nx<0 || map[nx][y]==2) {
-					flag =false;
-					return;
-				}else {
-					if(map[nx][y]==1) {
-						map[nx][y]=2;
-					}else {
-						map[x][y]=0;
-					}
-				}
-				cx = nx;
-				cy = y;
-			}else if(dir ==1) {
-				int ny = y++;
-				if(ny>=N || map[x][ny]==2) {
-					flag =false;
-					return;
-				}else {
-					if(map[x][ny]==1) {
-						map[x][ny]=2;
-					}else {
-						map[x][y]=0;
-					}
-				}
-				cx = x;
-				cy = ny;
-
-			}else if(dir ==2) {
-				int nx = x++;
-				if(nx>=N || map[nx][y]==2) {
-					flag = false;
-					return;
-				}else {
-					if(map[nx][y]==1) {
-						map[nx][y]=2;
-					}else {
-						map[x][y]=0;
-					}
-				}
-				cx = nx;
-				cy = y;
-
-			}else if(dir ==3) {
-				int ny = y--;
-				if(ny<0 || map[x][ny]==2) {
-					flag = false;
-					return;
-				}else {
-					if(map[x][ny]==1) {
-						map[x][ny]=2;
-					}else {
-						map[x][y]=0;
-					}
-				}
-				cx = x;
-				cy = ny;
-
-			}
-			sec++;
-		}
-		if(C=='L') dir = (dir+3)%4;
-		else if(C=='D') dir =(dir+1)%4;
-		
-	}
+	static Deque<Point> dq = new ArrayDeque<>();
+	static Queue<Point> moves = new LinkedList<>();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-			flag = false;
 		N = sc.nextInt();
 		K = sc.nextInt();
 		map = new int[N][N];
+		dir = 3;
+		dq.offer(new Point(0,0));
+		map[0][0]=2;
+		
 		for(int i=0; i<K; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			map[a-1][b-1]=1;
+			map[a-1][b-1] = 1;
 		}
-		flag = true;
-		dir =1;
-		sec = 0;
-		L = sc.nextInt();
-		for(int l=0; l<L; l++) {
-			int X = sc.nextInt();
-			char C = sc.next().charAt(0);
-			if(flag) 
-				move(X,C,cx,cy);
+		
+		int L = sc.nextInt();
+		for(int i=0; i<L; i++) {
+			int x = sc.nextInt();
+			int ic;
+			char c = sc.next().charAt(0);
+			if(c=='L') ic = 0;
+			else ic = 1;
 			
+			moves.add(new Point(x,ic));
+		}
+		int sec = 0;
+		int X = moves.peek().x;
+		int C = moves.poll().y;
+		
+		while(true) {
+			sec++;
+			int nx = dq.peekFirst().x+dx[dir];
+			int ny = dq.peekFirst().y+dy[dir];
+			
+			if(nx < 0 || nx>= N || ny<0 || ny>=N || map[nx][ny]==2) {
+				break;
+			}
+			
+			if(map[nx][ny]==1) {
+				map[nx][ny]=2;
+				dq.offerFirst(new Point(nx,ny));
+			}else {
+				map[nx][ny]=2;
+				dq.offerFirst(new Point(nx,ny));
+				map[dq.peekLast().x][dq.pollLast().y]=0;
+			}
+			if(sec == X) {
+				if(C==0) {
+					dir = (dir+1)%4;
+				}else {
+					dir = (dir+3)%4;
+				}
+				if(!moves.isEmpty()) {
+					X = moves.peek().x;
+					C = moves.poll().y;
+				}
+			}
 		}
 		System.out.println(sec);
+		
 	}
 }
